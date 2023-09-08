3ea5153fcb9030252d95d2d704b7916b6fc2d148
Eusebio Simango <esimango@technoplus.co.mz>
2023-09-08 15:18:41 +0200

    feat: add discount

---
+++ lab3celia/exercicios-propostos/4-desconto.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  float preco, precoComDesconto;
+
+  printf("Digite o preço do produto: ");
+  scanf("%f", &preco);
+
+  if (preco > 1000)
+  {
+    precoComDesconto = preco - (preco * 0.10);
+    printf("Preço com desconto:  %.2f mzn\n", precoComDesconto);
+  }
+  else
+  {
+    printf("Preço sem desconto: %.2f mzn\n", preco);
+  }
+
+  return 0;
+}
